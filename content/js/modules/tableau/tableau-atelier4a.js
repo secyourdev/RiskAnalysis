13687c2523cc3487cdca323e79264b52e07281b0
anthonylafourcade <36070812+anthonylafourcade@users.noreply.github.com>
2020-05-27 11:50:08 +0200

    Atelier 3 et 4

---
+++ content/js/modules/tableau/tableau-atelier4a.js
@@ -0,0 +1,11 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+    $('#dataTable1').DataTable( {
+        "paging":   false,
+        "info":     false
+    } );
+    $('#dataTable2').DataTable( {
+        "paging":   false,
+        "info":     false
+    } );
+} );
