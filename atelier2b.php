143ee0f1ede7d186f69ab5436f08a3533017d79d
anthonylafourcade <36070812+anthonylafourcade@users.noreply.github.com>
2020-06-10 09:53:45 +0200

    Modifs merge

--- atelier2b.php
+++ atelier2b.php
@@ -368,12 +368,7 @@
               <div class="card shadow mb-4">
                 <!-- Card Header - Dropdown -->
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-<<<<<<< HEAD:Atelier2b.html
                   <h6 class="m-0">Objectif</h6>
-                  
-=======
-                  <h6 class="m-0 font-weight-bold text-primary">Objectif</h6>
->>>>>>> origin/Joyston:atelier2b.php
                 </div>
                 <!-- Card Body -->
                 <div class="card-body">
