c4bbea36cffa826841f964fd39760cc7e75aa22b
anthonylafourcade <36070812+anthonylafourcade@users.noreply.github.com>
2020-07-10 11:42:02 +0200

    Atelier2 BDDv17

--- content/php/atelier2c/chart.php
+++ content/php/atelier2c/chart.php
@@ -3,7 +3,7 @@ session_start();
 $getid_projet = $_SESSION['id_projet'];
 header('Content-Type: application/json');
 
-$connect = mysqli_connect("mysql-ebios-rm.alwaysdata.net", "ebios-rm", 'hLLFL\bsF|&[8=m8q-$j', "ebios-rm_v14");
+$connect = mysqli_connect("mysql-ebios-rm.alwaysdata.net", "ebios-rm", 'hLLFL\bsF|&[8=m8q-$j', "ebios-rm_v17");
 
 $query_SROV = "SELECT description_source_de_risque, objectif_vise, pertinence, choix_source_de_risque FROM SROV WHERE id_projet = $getid_projet ORDER BY id_source_de_risque";
 
@@ -15,7 +15,7 @@ foreach ($result_SROV as $row) {
   if ($row['pertinence'] === "Faible"){
     $pertinence = "1";
   }
-  elseif ($row['pertinence'] === "Faible"){
+  elseif ($row['pertinence'] === "Moyen"){
     $pertinence = "2";
   }
   else{
