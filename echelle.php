48944fa93faf3d8bcda4eec07ace90ede1cd23d4
Joyston <joyston.antonraveendran@edu.esiee.fr>
2020-07-13 10:21:51 +0200

    Suppression_https_vers_donnees_locales

--- echelle.php
+++ echelle.php
@@ -12,7 +12,7 @@
 
   <!-- Fonts-->
   <link href="content/vendor/fontawesome-free/css/all.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+  <link href="content/fonts/nunito.css" rel="stylesheet">
 
   <!-- CSS -->
   <link href="content/css/bootstrap.css" rel="stylesheet">
