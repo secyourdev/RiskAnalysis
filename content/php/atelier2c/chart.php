50cfe58109471f8cb9386c987cbe5125541019a2
Carlos <carlos.pinto@secyourdev.com>
2020-07-14 23:21:24 +0200

    OK

--- content/php/atelier2c/chart.php
+++ content/php/atelier2c/chart.php
@@ -5,13 +5,13 @@ header('Content-Type: application/json');
 
 $connect = mysqli_connect("mysql-ebios-rm.alwaysdata.net", "ebios-rm", 'hLLFL\bsF|&[8=m8q-$j', "ebios-rm_v18");
 
-$query_SROV = "SELECT description_source_de_risque, objectif_vise, pertinence, choix_source_de_risque FROM SROV WHERE id_projet = $getid_projet ORDER BY id_source_de_risque";
+$query_SROV = "SELECT profil_de_l_attaquant_source_de_risque, objectif_vise, pertinence, choix_source_de_risque FROM SROV WHERE id_projet = $getid_projet ORDER BY id_source_de_risque";
 
 $result_SROV = mysqli_query($connect, $query_SROV);
 
 $data_SROV = array();
 foreach ($result_SROV as $row) {
-  $SROV = ($row['description_source_de_risque'] . " / " . $row['objectif_vise']);
+  $SROV = ($row['profil_de_l_attaquant_source_de_risque'] . " / " . $row['objectif_vise']);
   if ($row['pertinence'] === "Faible"){
     $pertinence = "1";
   }
