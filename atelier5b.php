a822ade2e2b597c22d22ad5ef34f442ee02f22c6
Joyston <joyston.antonraveendran@edu.esiee.fr>
2020-07-24 16:21:58 +0200

    maj_nom_sous_menu_atelier5b

--- atelier5b.php
+++ atelier5b.php
@@ -637,7 +637,7 @@ if (isset($_GET['id_utilisateur']) and $_GET['id_utilisateur'] > 0) {
                     <div class="card shadow mb-4">
                       <!-- Card Header - Dropdown -->
                       <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                        <h6 class="m-0"></h6>
+                        <h6 class="m-0">Tableau récapitulatif</h6>
                       </div>
                       <!-- Card Body -->
                       <div class="card-body">
