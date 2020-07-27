500ae7c88677928093d7c8aaaeece449caa248c6
michelg <gumichel1@gmail.com>
2020-07-27 14:47:39 +0200

    cloisonnement id_projet et id_atelier 1d

--- content/php/atelier1d/ajout_regle.php
+++ content/php/atelier1d/ajout_regle.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 $getid_projet = $_SESSION['id_projet'];
-
+print $getid_projet;
 include("../bdd/connexion.php");
 
 $results["error"] = false;
@@ -18,8 +18,8 @@ $dates = '';
 $recupere_id_socle = $bdd->prepare("SELECT id_socle_securite FROM N_socle_de_securite WHERE N_socle_de_securite.nom_referentiel = ? AND id_atelier = '1.d' AND id_projet = $getid_projet");
 
 $insere_regle = $bdd->prepare(
-  "INSERT INTO O_regle(id_regle, id_regle_affichage, titre, description, etat_de_la_regle, justification_ecart, dates, responsable, id_socle_securite) 
-VALUES ('',?,?,?,?,?,?,?,?)"
+  "INSERT INTO O_regle(id_regle, id_regle_affichage, titre, description, etat_de_la_regle, justification_ecart, dates, responsable, id_socle_securite, id_projet) 
+VALUES ('',?,?,?,?,?,?,?,?, '1.d', $getid_projet)"
 );
 
 // Verification du nom_referentiel
@@ -51,6 +51,7 @@ if ($results["error"] === false && isset($_POST['validerecart'])) {
   $recupere_id_socle->bindParam(1, $nom_referentiel);
   $recupere_id_socle->execute();
   $id_socle_securite = $recupere_id_socle->fetch();
+  print $id_socle_securite[0];
 
   $insere_regle->bindParam(1, $id_regle_affichage);
   $insere_regle->bindParam(2, $titre);
@@ -64,5 +65,5 @@ if ($results["error"] === false && isset($_POST['validerecart'])) {
   $_SESSION['message_success_2'] = "La règle a bien été ajoutée !";
 }
 
-header('Location: ../../../atelier-1d&' . $_SESSION['id_utilisateur'] . '&' . $_SESSION['id_projet'].'#regles');
+// header('Location: ../../../atelier-1d&' . $_SESSION['id_utilisateur'] . '&' . $_SESSION['id_projet'].'#regles');
 ?>
