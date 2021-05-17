4b7d39822b08f5e4cc3e85be194e571f144cb310
InelG <ghazliinel@gmail.com>
2021-05-17 21:46:14 +0200

    Correction de 1.d (pas de style dyn)

--- content/php/atelier1d/selection.php
+++ content/php/atelier1d/selection.php
@@ -19,6 +19,6 @@ $result_nom_referentiel2 = mysqli_query($connect, $query_nom_referentiel);
 $rq_socle_sec = "SELECT type_referentiel AS 'Type de référentiel', nom_referentiel AS 'Nom du référentiel', etat_d_application AS 'État d''application' FROM N_socle_de_securite WHERE id_projet = $getid_projet";
 $rq_socle_sec_tab = mysqli_query($connect, $rq_socle_sec);
 
-$rq_regle = "SELECT id_regle AS 'ID de la règle', titre AS 'Titre de la règle', description AS 'Description de la règle', etat_de_la_regle AS 'État de la règle', justification_ecart AS 'Justification des écarts', responsable AS 'Responsable', dates AS 'Date limite de la mise en application' FROM O_regle WHERE id_projet = $getid_projet";
+$rq_regle0 = "SELECT id_regle_affichage AS 'ID de la règle', titre AS 'Titre de la règle', description AS 'Description de la règle', etat_de_la_regle AS 'État de la règle', justification_ecart AS 'Justification des écarts', responsable AS 'Responsable', dates AS 'Date limite de la mise en application' FROM O_regle WHERE id_projet = $getid_projet";
 
-$rq_regle_tab = mysqli_query($connect, $rq_regle);
+$rq_regle_tab = mysqli_query($connect, $rq_regle0);
