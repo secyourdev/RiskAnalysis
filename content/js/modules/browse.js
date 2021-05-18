7c490a79cf1e81fe407d969412d2f9edeb4987f8
InelG <ghazliinel@gmail.com>
2021-05-18 13:27:32 +0200

    delete

--- content/js/modules/browse.js
+++
@@ -1,6 +0,0 @@
-$('.custom-file-input').on('change', function () {
-    var fileName_path = $(this).val()
-    var fileName = fileName_path.substring(fileName_path.lastIndexOf('\\') + 1)
-    
-    $(this).next('.custom-file-label').addClass("selected").html(fileName)
-})
--- content/php/atelier2b/selection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$getid_projet = intval($_GET['id_projet']);
-include("content/php/bdd/connexion_sqli.php");
-$query = "SELECT * FROM P_SROV WHERE id_projet = $getid_projet";
-
-$result = mysqli_query($connect, $query);
-
-?>
