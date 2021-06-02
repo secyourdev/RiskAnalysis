adcb57035de3f0235a3db710d30ee7a11abdc241
InelG <ghazliinel@gmail.com>
2021-06-02 14:14:22 +0200

    ajout à gitignore

--- content/php/bdd/connexion.php
+++ content/php/bdd/connexion.php
@@ -1,9 +1,9 @@
 <?php
 try{
     //Serveur Distant
-    $bdd=new PDO('mysql:host=mysql-ebios-rm.alwaysdata.net;dbname=ebios-rm_v29;charset=utf8','ebios-rm_admin','2HW8RYULUtnjkCD0pRtngaigq9J0',
+    // $bdd=new PDO('mysql:host=mysql-ebios-rm.alwaysdata.net;dbname=ebios-rm_v29;charset=utf8','ebios-rm_admin','2HW8RYULUtnjkCD0pRtngaigq9J0',
     //Serveur Local
-    //$bdd=new PDO('mysql:host=localhost;dbname=test2;charset=utf8','root','',
+    $bdd=new PDO('mysql:host=localhost;dbname=bdd;charset=utf8','root','',
     array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 }
 
