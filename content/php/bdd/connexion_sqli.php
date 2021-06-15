e47ccebe6f90b7aa38c7cd503e1e5b17e846b3bb
test <test@test.com>
2021-06-15 23:22:58 +0200

    OK

--- content/php/bdd/connexion_sqli.php
+++ content/php/bdd/connexion_sqli.php
@@ -1,8 +1,8 @@
 <?php
 // Serveur Distant 
-// $connect = mysqli_connect("mysql-ebios-rm.alwaysdata.net", "ebios-rm_admin", '2HW8RYULUtnjkCD0pRtngaigq9J0', "ebios-rm_v29");
+ $connect = mysqli_connect("mysql-ebios-rm.alwaysdata.net", "ebios-rm_admin", '2HW8RYULUtnjkCD0pRtngaigq9J0', "ebios-rm_v29");
 // Serveur Local 
-$connect = mysqli_connect("localhost", "root", '', "bdd");
-mysqli_set_charset($connect, "utf8");
+//$connect = mysqli_connect("localhost", "root", '', "bdd");
+//mysqli_set_charset($connect, "utf8");
 
 ?>
